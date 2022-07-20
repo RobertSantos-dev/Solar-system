156b46742ec8721d41d50f3260d6c41d09887d07
Robert Santos <robertdosantos09@gmail.com>
2022-07-20 16:20:52 -0300

    Resolução dos requisitos 3 e 4, criando um componente de nome Title e importando para o SolarSystem.

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Title from './Title';
+
 export default class SolarSystem extends React.Component {
   render() {
-    return <div data-testid="solar-system"> </div>;
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
+    );
   }
 }
