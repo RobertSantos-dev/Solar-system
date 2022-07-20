e6ae808b483069c0d00071c6ee899d6a41319559
Robert Santos <robertdosantos09@gmail.com>
2022-07-20 17:38:58 -0300

    Resolução do 6° requisito, inserindo uma lista dos planetas

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 export default class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          {planets.map((e, i) => (
+            <li key={ i }>
+              <PlanetCard planetName={ e.name } planetImage={ e.image } />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
