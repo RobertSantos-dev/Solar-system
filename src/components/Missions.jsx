036383d85faeb9ce25a032a0e03598acf3cd00e6
Robert Santos <robertdosantos09@gmail.com>
2022-07-20 19:21:42 -0300

    Resolução do 10° requisito, inserindo uma lista no Missions usando o componente MissionCard.

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
 import Title from './Title';
-// import MissionCard from './MissionCard';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 export default class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <ul>
+          {missions.map((e, i) => {
+            const { name, year, country, destination } = e;
+            return (
+              <li key={ i }>
+                <MissionCard
+                  name={ name }
+                  year={ year }
+                  country={ country }
+                  destination={ destination }
+                />
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
